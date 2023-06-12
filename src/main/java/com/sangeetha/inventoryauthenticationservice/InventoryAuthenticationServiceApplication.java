aedb02a7f25c3e48dc369a32a324667275b695f3
sangeetha <raok.sangeetha@gmail.com>
2023-06-12 11:11:44 +0530

    Inventory management application

---
+++ src/main/java/com/sangeetha/inventoryauthenticationservice/InventoryAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sangeetha.inventoryauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangeetha/inventoryauthenticationservice/InventoryAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangeetha.inventoryauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
